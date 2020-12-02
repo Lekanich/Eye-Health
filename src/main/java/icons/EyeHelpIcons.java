efba30916cb50e90362b8c2b1c4a81508803f7d5
lekanich <lekan1992@gmail.com>
2020-12-02 20:54:56 +0200

    * IDEA update [2020.3]

--- src/main/java/icons/EyeHelpIcons.java
+++ src/main/java/icons/EyeHelpIcons.java
@@ -8,6 +8,6 @@ import com.intellij.openapi.util.IconLoader;
  * @author Lekanich
  */
 public class EyeHelpIcons {
-	public static final Icon EYE_ON = IconLoader.getIcon("/icons/action_eye.svg");
-	public static final Icon EYE_OFF = IconLoader.getIcon("/icons/inactive_action_eye.svg");
+	public static final Icon EYE_ON = IconLoader.getIcon("/icons/action_eye.svg", EyeHelpIcons.class);
+	public static final Icon EYE_OFF = IconLoader.getIcon("/icons/inactive_action_eye.svg", EyeHelpIcons.class);
 }
