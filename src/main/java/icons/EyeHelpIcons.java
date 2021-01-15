3ec5411652e6d2ca40488ce94412869f4d1ec142
lekanich <lekan1992@gmail.com>
2021-01-16 00:52:22 +0200

    * two new icons: winter and autumn

--- src/main/java/icons/EyeHelpIcons.java
+++ src/main/java/icons/EyeHelpIcons.java
@@ -2,12 +2,17 @@ package icons;
 
 import javax.swing.Icon;
 import com.intellij.openapi.util.IconLoader;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 
 /**
  * @author Lekanich
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EyeHelpIcons {
 	public static final Icon EYE_ON = IconLoader.getIcon("/icons/action_eye.svg", EyeHelpIcons.class);
+	public static final Icon EYE_ON_WINTER = IconLoader.getIcon("/icons/action_eye_winter.svg", EyeHelpIcons.class);
+	public static final Icon EYE_ON_AUTUMN = IconLoader.getIcon("/icons/action_eye_winter.svg", EyeHelpIcons.class);
 	public static final Icon EYE_OFF = IconLoader.getIcon("/icons/inactive_action_eye.svg", EyeHelpIcons.class);
 }
