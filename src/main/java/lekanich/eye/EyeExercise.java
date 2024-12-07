422975d9b66de399bd7c9332749cc90a614362c1
Oleksandr Zhelezniak <lekan1992@gmail.com>
2024-12-07 23:43:40 +0200

    chore: remove useless code

--- src/main/java/lekanich/eye/EyeExercise.java
+++ src/main/java/lekanich/eye/EyeExercise.java
@@ -8,13 +8,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.ui.ColorUtil;
 import com.intellij.util.ResourceUtil;
 import com.intellij.util.io.URLUtil;
-import com.intellij.util.ui.StartupUiUtil;
-import com.intellij.util.ui.UIUtil;
 import com.intellij.util.xmlb.annotations.Attribute;
-import org.jetbrains.annotations.NotNull;
 
 
 /**
@@ -42,41 +38,20 @@ public record EyeExercise(@Attribute("file") String fileName) {
 		final String dummyMessage = "Please report this to plugin provider";
 		try {
 			final StringBuilder text = new StringBuilder();
-			String cssText;
 			final File file = new File(fileName);
 			if (file.isAbsolute() && file.exists()) {
 				text.append(FileUtil.loadFile(file));
-				final URL cssResource = cssResource();
-				cssText = ResourceUtil.loadText(URLUtil.openStream(cssResource));
 			} else {
 				final InputStream stream = ResourceUtil.getResourceAsStream(getClass().getClassLoader(), MAIN_FOLDER, fileName);
 				if (stream == null) {
 					return dummyMessage;
 				}
 				text.append(ResourceUtil.loadText(stream));
-				final String cssFileName = isDark() ? "css/tips_darcula.css" : "css/tips.css";
-				final InputStream cssResourceStream = ResourceUtil.getResourceAsStream(getClass().getClassLoader(), "/tips/", cssFileName);
-				cssText = cssResourceStream != null ? ResourceUtil.loadText(cssResourceStream) : "";
 			}
 
-			final String inlinedCSS = cssText + "\nbody {background-color:#" + ColorUtil.toHex(UIUtil.getTextFieldBackground()) + ";overflow:hidden;}";
-			return text.toString().replaceFirst("<link.*\\.css\">", "<style type=\"text/css\">\n" + inlinedCSS + "\n</style>");
+			return text.toString();
 		} catch (IOException e) {
 			return dummyMessage;
 		}
-	}
-
-	public static URL cssResource() {
-		final String cssFileName = isDark() ? "exercise_darcula.css" : "exercise.css";
-		return ResourceUtil.getResource(EyeExercise.class.getClassLoader(), "/exercises/css/", cssFileName);
-	}
-
-	@NotNull
-	public static String cssResourceFileName() {
-		return "exercises/css/" + (isDark() ? "exercise_darcula.css" : "exercise.css");
-	}
-
-	private static boolean isDark() {
-		return StartupUiUtil.INSTANCE.isDarkTheme();
 	}
 }
