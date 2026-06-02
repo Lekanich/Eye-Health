2a4ee5cff192829a2359e614ca75be56a20d7d07
Oleksandr Zhelezniak <lekan1992@gmail.com>
2026-06-02 19:31:33 +0300

    fix: find my gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,15 +2,19 @@ rootProject.name = "Eye-Health"
 
 pluginManagement {
 	repositories {
-		maven("https://oss.sonatype.org/content/repositories/snapshots/")
+		maven("https://oss.sonatype.org/content/repfixositories/snapshots/")
 		gradlePluginPortal()
 		mavenCentral()
 		maven {
 			name = "GitHubPackages"
 			url = uri("https://maven.pkg.github.com/lekanich/GradlePlugins")
 			credentials {
-				username = providers.environmentVariable("GITHUB_ACTOR").orNull
-				password = providers.environmentVariable("GITHUB_TOKEN").orNull
+				username =
+					providers.environmentVariable("GITHUB_ACTOR").orNull
+						?: providers.environmentVariable("GH_ACTOR").orNull
+				password =
+					providers.environmentVariable("GITHUB_TOKEN").orNull
+						?: providers.environmentVariable("GH_TOKEN").orNull
 			}
 		}
 	}
